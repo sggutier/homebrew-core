bf2e9c7fdb0c7edb14ef25bcf3ff63639668e9d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-17 00:42:52 +0000

    grpc 1.65.1

--- Formula/g/grpc.rb
+++ Formula/g/grpc.rb
@@ -2,13 +2,12 @@ class Grpc < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://grpc.io/"
   license "Apache-2.0"
-  revision 2
   head "https://github.com/grpc/grpc.git", branch: "master"
 
   stable do
     url "https://github.com/grpc/grpc.git",
-        tag:      "v1.62.2",
-        revision: "96f984744fe728e196c11d33b91b022566c0d40f"
+        tag:      "v1.65.1",
+        revision: "4b206a2812af345297a7223026cd7c5fd643d3be"
 
     # Backport fix for Protobuf 26
     patch do
