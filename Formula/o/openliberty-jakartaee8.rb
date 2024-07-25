98e11e127785ef112c6bde2b2b2319aff1295f1f
Issy Long <me@issylong.com>
2024-07-25 21:52:05 +0100

    openliberty-jakartaee8: Fix `Homebrew/NoFileutilsRmrf` RuboCop offenses

--- Formula/o/openliberty-jakartaee8.rb
+++ Formula/o/openliberty-jakartaee8.rb
@@ -17,7 +17,7 @@ class OpenlibertyJakartaee8 < Formula
   depends_on "openjdk"
 
   def install
-    rm_rf Dir["bin/**/*.bat"]
+    rm_r(Dir["bin/**/*.bat"])
 
     libexec.install Dir["*"]
     (bin/"openliberty-jakartaee8").write_env_script "#{libexec}/bin/server",
